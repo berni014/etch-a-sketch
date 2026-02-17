93ba3e18ef149433df2544425bb56aace89d5074
Berni <192388124+berni014@users.noreply.github.com>
2026-02-17 10:37:52 -0300

    All done

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const container= document.getElementById("container");
+const btn= document.getElementById("btn");
 
-let totalCuadrados= 16*16;
 
-for (let i = 0 ;i < totalCuadrados; i++ ) {
+function createGrid(size){
+    container.innerHTML="";
+for (let i = 0 ;i < size*size; i++ ) {
     
+
     let miniDiv = document.createElement("div");
     miniDiv.classList.add("square")
 
     miniDiv.addEventListener("mouseover",() => {
-        miniDiv.style.backgroundColor= "blue";
+        const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    // Aplicamos el color usando Template Literals
+    miniDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     })
 
     container.appendChild(miniDiv)
 }
+}
 
+btn.addEventListener("click",()=>{
+    let userSize= prompt("how many squares per side do you want(max 100)");
+    userSize= parseInt(userSize);
+    if(userSize>0 && userSize<= 100){
+        createGrid(userSize);
+    }
+    else {
+        alert("Elige un numero entre 1 y 100.")
+    }
 
+})
+
+createGrid(16);
